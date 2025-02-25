81ebd86fce2db479818169351bed25962e24b71b
HelloImSak <povsamsung35@gmail.com>
2025-02-25 14:56:36 +0700

    add: added library

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import "./App.css";
-import { IconCloudDemo } from "./components/ui_com/IconCloudDemo";
+import Navbar1 from "./components/Navbar1";
 
 function App() {
   return (
     <>
-      <IconCloudDemo />
-      <p className="font-OpenSanSemiBold text-2xl text-primary">Hello</p>
-      <p className="font-OpenSanSemiBold text-2xl text-secondary">Hello</p>
-      <p className="font-OpenSanSemiBold text-2xl text-accent_1">Hello</p>
-      <p className="font-OpenSanSemiBold text-2xl text-accent_2">Hello</p>
-      <p className="font-OpenSanSemiBold text-2xl text-accent_3">Hello</p>
-      <p className="font-OpenSanSemiBold text-2xl text-accent_4">Hello</p>
-      <p className="font-OpenSanSemiBold text-2xl text-black_50">Hello</p>
+      <Navbar1 />
     </>
   );
 }
