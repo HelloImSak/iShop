15c9ed3e227428af4320d7a71d56c5482ff0a117
HelloImSak <povsamsung35@gmail.com>
2025-02-25 16:40:27 +0700

    add: added redux

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -3,12 +3,14 @@ const Navbar1 = () => {
   return (
     <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div class="">
-        <p className="text-small bg-primary text-white p-1 flex justify-center items-center">Get 30% off when you spend over $200.</p>
+        <p className="text-small bg-primary text-white p-1 flex justify-center items-center">
+          Get 30% off when you spend over $200.
+        </p>
       </div>
-      <div class="flex flex-wrap items-center justify-between mx-[50px] py-2">
+      <div class="flex flex-row items-center justify-between mx-[50px] py-2">
         <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
           <img
-          class="w-[120px] h-[45px]"
+            class="w-[120px] h-[45px]"
             src="./src/assets/ishop-dark-logo.png"
             alt="Flowbite Logo"
           />
@@ -19,24 +21,7 @@ const Navbar1 = () => {
           class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-multi-level"
           aria-expanded="false"
-        >
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
+        ></button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-multi-level">
           <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
@@ -97,7 +82,6 @@ const Navbar1 = () => {
                     >
                       Dropdown
                     </button>
-                    
                   </li>
                   <li>
                     <a
@@ -108,7 +92,6 @@ const Navbar1 = () => {
                     </a>
                   </li>
                 </ul>
-                
               </div>
             </li>
             <li>
@@ -137,6 +120,47 @@ const Navbar1 = () => {
             </li>
           </ul>
         </div>
+
+        <form class="w-[500px]">
+          <label
+            for="default-search"
+            class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >
+            Search
+          </label>
+          <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search products..."
+              required
+            />
+          </div>
+        </form>
+        <button
+          type="button"
+          class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        >
+          Alternative
+        </button>
       </div>
     </nav>
   );
