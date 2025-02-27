9ac98642f9604454aed81a6c8bc6a92087024423
John Doe <choeunsela332@gmail.com>
2025-02-27 15:07:31 +0700

    added card

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import "./App.css";
-<<<<<<< HEAD
-import AppRoutes from "./routes/AppRoutes";
 
-function App() {
-  return <AppRoutes />;
-=======
+import CardComponnets from "./components/CardComponnets";
+import CardDisComponnets from "./components/CardDisComponnets";
+
+
 import NavbarTwoComponent from "./components/NavbarTwoComponent";
 // import Routes from "./routes/routes";
 import NotFoundProduct from "./components/Not-Found-Product";
 
 // import NotFoundProduct from "./components/Not-Found-Product";
-
-export default function App() {
+function App() {
   return (
     <div>
-      <NotFoundProduct />
-      {/* <NavbarTwoComponent /> */}
+      <CardDisComponnets/> 
     </div>
+    
   );
->>>>>>> a816a1ce38d78159fd38deffa7c2c8e6deb262cd
 }
+
+export default App;
+ 
