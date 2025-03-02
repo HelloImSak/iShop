10407ef52a528c4f577dcceac7f7a7a8cc7614e7
ChannimEY <channimey1@gmail.com>
2025-03-02 22:57:54 +0700

    add: update com

--- src/components/Discount/LayoutCardCom.jsx
+++ src/components/Discount/LayoutCardCom.jsx
@@ -3,14 +3,17 @@ import CardDisComponnets from "../CardDisComponnets";
 
 export default function LayoutCardCom() {
   return (
-    <main>
-      <section className="grid grid-cols-3 gap-x-[30px] gap-y-[30px] w-3/4 p-4">
-        <CardDisComponnets />
-        <CardDisComponnets />
-        <CardDisComponnets />
-        <CardDisComponnets />
-        <CardDisComponnets />
-        <CardDisComponnets />
+    <main className="flex items-center justify-center h-screen  ">
+      <section className="grid grid-cols-3 gap-x-[30px] gap-y-[30px] w-full max-w-[1000px] h-screen p-4">
+       <CardDisComponnets/>
+       <CardDisComponnets/>
+       <CardDisComponnets/>
+       <CardDisComponnets/>
+       <CardDisComponnets/>
+       <CardDisComponnets/>
+       <CardDisComponnets/>
+       <CardDisComponnets/>
+       <CardDisComponnets/>
       </section>
     </main>
   );
