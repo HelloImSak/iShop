2631ca7cbc5edca6ed4f1b0ea1f01f445c6868c5
John Doe <choeunsela332@gmail.com>
2025-03-03 14:16:43 +0700

    Merge branch 'dev' of https://github.com/HelloImSak/iShop into seyla

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import { Route } from "react-router";
 import "./App.css";
 
-import CardComponnets from "./components/CardComponnets";
-
-
-
-import NotFoundProduct from "./components/Not-Found-Product";
 function App() {
-  return (
-    <div>
-      <CardDisComponnets/> 
-    </div>
-    
-  );
+  return(
+
+  )
 }
 
 export default App;
